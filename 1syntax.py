c23da709134a6ff1422ded7d9c5878d6223f44a9
rajibscode <rajibsarder321@gmail.com>
2022-10-02 23:24:11 +0600

    first python code start

---
+++ 1syntax.py
@@ -0,0 +1,21 @@
+# using multiline
+# comment
+
+# using print function
+print("hello python")
+
+print(123)
+
+# using type function
+a=100
+print(type(a))
+
+b="hi i am rajib"
+print(type(b))
+
+c=236.58
+print(type(c))
+
+# using input function
+a = input("Enter Your Name: ")
+print("your name is",a)
